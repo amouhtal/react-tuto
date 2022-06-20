b76ea452bd40aa1c5abb7c2d6d7ba6b40fb67d46
amouhtal <mouhtal.abderrahmane@gmail.com>
2022-06-20 14:33:52 +0100

    Full React Tutorial #10 - Outputting Lists

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import {useState} from 'react'
+
+const Home = () => {
+
+    const [blogs,setBlogs] = useState(
+        [
+            {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+            {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+            {title: 'web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3},
+        ]
+    );
+
+     return (
+         <div  className="home">
+         </div>
+     );
+ }
+
+ export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () =>{
+    return (
+        <nav className="navbar">
+            <h1>The Dojo Blog </h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/Create">New Blog</a>
+
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
